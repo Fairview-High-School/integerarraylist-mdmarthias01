b7d7848129bc79f935f9d426c12b19103b06a254
mdmarthias01 <mdmarthias01@bvsd.org>
2024-09-26 21:34:58 +0000

    DOne

--- IntegerArrayList.java
+++ IntegerArrayList.java
@@ -1,113 +1,116 @@
-public class IntegerArrayList implements IntegerList{
-    private Integer[]data;
+public class IntegerArrayList implements IntegerList {
+    private Integer[] data;
     private int size;
 
-    public IntegerArrayList(){
+    public IntegerArrayList() {
         data = new Integer[10];
         size = 0;
     }
 
-
-     public void add(Integer val){
-         if (size >= data.length){
+    public void add(Integer val) {
+        if (size >= data.length) {
             Integer[] biggerArray = new Integer[data.length * 2];
-            for(int i=0; i<data.length; i++){
+            for (int i = 0; i < data.length; i++) {
                 biggerArray[i] = data[i];
             }
-            data =biggerArray;
-            
-            
-         }
+            data = biggerArray;
+        }
         data[size] = val;
-        size ++; 
-     }
-
-
-    public void add(int index, Integer val){
-        throw new IndexOutOfBoundsException();
-
+        size++;
     }
 
-
-
-    public void set(int index, Integer val){
-        throw new IndexOutOfBoundsException();
-
+    public void add(int index, Integer val) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (size >= data.length) {
+            Integer[] biggerArray = new Integer[data.length * 2];
+            for (int i = 0; i < data.length; i++) {
+                biggerArray[i] = data[i];
+            }
+            data = biggerArray;
+        }
+        for (int i = size; i > index; i--) {
+            data[i] = data[i - 1];
+        }
+        data[index] = val;
+        size++;
     }
 
-
-
-
-    public void clear(){
-        throw new IndexOutOfBoundsException();
-
+    public void set(int index, Integer val) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        data[index] = val;
     }
 
-
-
-
-
-    public Integer remove(int index){
-        throw new IndexOutOfBoundsException();
-
+    public void clear() {
+        data = new Integer[10];
+        size = 0;
     }
 
+    public Integer remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Integer removed = data[index];
+        for (int i = index; i < size - 1; i++) {
+            data[i] = data[i + 1];
+        }
+        data[size - 1] = null;
+        size--;
+        return removed;
+    }
 
-
-    public Integer get(int index){
-        if(index < 0 || index >= size){
-            throw new IndexOutOfBoundsException(" Index: " + index);
+    public Integer get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index);
         }
         return data[index];
-        
-
     }
 
-
-
-    public int size(){
-        throw new IndexOutOfBoundsException();
-
+    public int size() {
+        return size;
     }
 
-
-    public boolean isEmpty(){
-        throw new IndexOutOfBoundsException();
-
+    public boolean isEmpty() {
+        return size == 0;
     }
 
-
-
-    public boolean contains(Integer val){
-        throw new IndexOutOfBoundsException();
+    public boolean contains(Integer val) {
+        return indexOf(val) != -1;
     }
 
-
-
-    public int indexOf(Integer val){
-        throw new IndexOutOfBoundsException();
-
+    public int indexOf(Integer val) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(val)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
-
-
-    public boolean equals(List<Integer> other){
-        throw new IndexOutOfBoundsException();
+    public boolean equals(List<Integer> other) {
+        if (other == null || other.size() != size) {
+            return false;
+        }
+        for (int i = 0; i < size; i++) {
+            if (!data[i].equals(other.get(i))) {
+                return false;
+            }
+        }
+        return true;
     }
 
-
-    public String toString(){
-        String result = "[";
-        for (int i = 0; i <size-1; i++){
-            result += data[i] + ",";
-        } 
-        
-        result +=data[size-1] + "]";
-        return result;
-
+    public String toString() {
+        if (size == 0) {
+            return "[]";
+        }
+        StringBuilder result = new StringBuilder("[");
+        for (int i = 0; i < size - 1; i++) {
+            result.append(data[i]).append(", ");
+        }
+        result.append(data[size - 1]).append("]");
+        return result.toString();
     }
-
-
-
-    
 }
--- Test.java
+++ Test.java
@@ -5,8 +5,8 @@ public class Test {
         // Initialize the list
         IntegerArrayList list = new IntegerArrayList();
 
-        System.out.println("Test add (val) and get (index): add 5 values to the list, and then get the 5 values from the list");
-        System.out.println("Expected: 10 20 30 40 50");
+        System.out.println("Test add(val) and get(index): add 5 values to the list, and then get the 5 values from the list");
+        System.out.println("Expected: [10, 20, 30, 40, 50]");
 
         // Add values to the list
         for (int val = 10; val <= 50; val += 10) {
@@ -20,11 +20,10 @@ public class Test {
         for (int i = 0; i < 5; i++) {
             System.out.print(list.get(i) + " ");
         }
-
-        System.out.println(); // New line
+        System.out.println("\n");
 
         System.out.println("Test add(val): add the 11th item to the list (causes an array resize)");
-        System.out.println("Expected: 10 20 30 40 50 60 70 80 90 100 110");
+        System.out.println("Expected: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110]");
 
         // Add more values, causing an array resize
         for (int val = 60; val <= 110; val += 10) {
@@ -33,5 +32,65 @@ public class Test {
 
         // Print the updated list
         System.out.println(list);
+        System.out.println();
+
+        System.out.println("Test add(index, val): add 25 at index 2");
+        System.out.println("Expected: [10, 20, 25, 30, 40, 50, 60, 70, 80, 90, 100, 110]");
+
+        // Add a value at index 2
+        list.add(2, 25);
+
+        // Print the list after insertion
+        System.out.println(list);
+        System.out.println();
+
+        System.out.println("Test set(index, val): set index 5 to 55");
+        System.out.println("Expected: [10, 20, 25, 30, 40, 55, 60, 70, 80, 90, 100, 110]");
+
+        // Set value at index 5
+        list.set(5, 55);
+
+        // Print the list after setting a value
+        System.out.println(list);
+        System.out.println();
+
+        System.out.println("Test remove(index): remove value at index 3");
+        System.out.println("Expected: [10, 20, 25, 40, 55, 60, 70, 80, 90, 100, 110]");
+
+        // Remove value at index 3
+        list.remove(3);
+
+        // Print the list after removing the value
+        System.out.println(list);
+        System.out.println();
+
+        System.out.println("Test size(): check the size of the list");
+        System.out.println("Expected size: 11");
+        System.out.println("Actual size: " + list.size());
+        System.out.println();
+
+        System.out.println("Test isEmpty(): check if the list is empty");
+        System.out.println("Expected: false");
+        System.out.println("Actual: " + list.isEmpty());
+        System.out.println();
+
+        System.out.println("Test contains(val): check if the list contains 55");
+        System.out.println("Expected: true");
+        System.out.println("Actual: " + list.contains(55));
+        System.out.println();
+
+        System.out.println("Test indexOf(val): check the index of 55");
+        System.out.println("Expected: 4");
+        System.out.println("Actual: " + list.indexOf(55));
+        System.out.println();
+
+        System.out.println("Test clear(): clear the list");
+        list.clear();
+        System.out.println("Expected: []");
+        System.out.println(list);
+
+        System.out.println("Test isEmpty() after clear(): check if the list is empty");
+        System.out.println("Expected: true");
+        System.out.println("Actual: " + list.isEmpty());
     }
 }
